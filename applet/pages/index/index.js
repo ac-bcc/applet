5268593a0041c3fef8492c3da30bc6f74313adbe
ac-bcc <87368762+ac-bcc@users.noreply.github.com>
2021-12-30 23:10:21 +0800

    Add files via upload

---
+++ applet/pages/index/index.js
@@ -0,0 +1,72 @@
+// index.js
+// 获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    motto: 'Hello World',
+    relationtxt: '计算机学科各课程关系图',
+    resourcetxt: '计算机学科各课程的参考资料',
+    knowtxt: '对计算机专业的认识',
+    plantxt: '学业规划',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    canIUseGetUserProfile: false,
+    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName') // 如需尝试获取用户信息可改为false
+  },
+  // 事件处理函数
+  btna(){
+    wx.navigateTo({
+      url: '../new1/new1',
+    })
+  },
+  btnb(){
+    wx.navigateTo({
+      url: '../new2/new2',
+    })
+  },
+  btnc(){
+    wx.navigateTo({
+      url: '../new3/new3',
+    })
+  },
+  btnd(){
+    wx.navigateTo({
+      url: '../new4/new4',
+    })
+  },
+  bindViewTap() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad() {
+    if (wx.getUserProfile) {
+      this.setData({
+        canIUseGetUserProfile: true
+      })
+    }
+  },
+  getUserProfile(e) {
+    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
+    wx.getUserProfile({
+      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+      success: (res) => {
+        console.log(res)
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    })
+  },
+  getUserInfo(e) {
+    // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
+    console.log(e)
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  }
+})
---
+++ applet/pages/new4/new4.js
@@ -0,0 +1,72 @@
+// pages/new4/new4.js
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        txt1:"提高自己的代码水平",
+        txt2:"打好理论基础，不断加深对底层逻辑的理解",
+        txt3:"多了解一些计算机学科的方向和具体工作",
+        txt4:"尽量在程序设计竞赛中取得一定成绩",
+        txt5:"在实验室进行科研训练",
+        txt6:"动手写一个操作系统"
+
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
+
+    }
+})
